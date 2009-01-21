37a996f758c3fe48c541c32ef201b16135284c60
jcea <jcea@localhost>
2009-01-21 15:27:33 +0000

    Getting ready for 4.6.4 release

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2008 Jesus Cea Avion'
 # The short X.Y version.
 version = '4.7.4'
 # The full version, including alpha/beta/rc tags.
-release = '4.7.4devel6'
+release = '4.7.4'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
