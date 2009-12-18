748d1db0d523146b28d104d941b757fb7425b2da
Jesus Cea <jcea@jcea.es>
2009-12-18 22:19:48 +0100

    Preparing 4.8.2 release

--- Modules/bsddb.h
+++ Modules/bsddb.h
@@ -105,7 +105,7 @@
 #error "eek! DBVER can't handle minor versions > 9"
 #endif
 
-#define PY_BSDDB_VERSION "4.8.2devel9"
+#define PY_BSDDB_VERSION "4.8.2"
 
 /* Python object definitions */
 
--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2008-2009 Jesus Cea Avion'
 # The short X.Y version.
 version = '4.8.2'
 # The full version, including alpha/beta/rc tags.
-release = '4.8.2devel6'
+release = '4.8.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
