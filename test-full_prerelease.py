5ac73ef4e2db0c167baa735da8cfc56032436f83
Jesus Cea <jcea@jcea.es>
2013-03-26 06:26:01 +0100

    Cleanup

--- test-full_prerelease.py
+++ test-full_prerelease.py
@@ -12,10 +12,10 @@ you try it as is, it would fail.
 """
 
 def do_matrix_check() :
-  python_versions=("2.4","2.5","2.6","2.7","3.1","3.2","3.3")
-  berkeleydb_versions=("4.3","4.4","4.5","4.6","4.7","4.8",
-                       "5.0","5.1","5.2","5.3",
-                       "6.0")
+  python_versions=('2.4', '2.5', '2.6', '2.7', '3.1', '3.2', '3.3')
+  berkeleydb_versions=('4.3', '4.4', '4.5', '4.6', '4.7', '4.8',
+                       '5.0', '5.1', '5.2', '5.3',
+                       '6.0')
 
   warning_level=("-Wdefault", "-Werror")[1]
   import subprocess
